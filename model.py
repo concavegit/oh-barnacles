5189d8c64180a2c83cb3d660fc35f68d8ac4a9e4
Kawin Nikomborirak <concavemail@gmail.com>
2017-09-26 19:42:10 +0000

    fix whelk csv

--- model.py
+++ model.py
@@ -2,3 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 plt.style.use('ggplot')
+
+palmerAlive = pd.read_csv('data/palmer-alive.csv')
+palmerDead = pd.read_csv('data/palmer-dead.csv')
+palmerTotal = pd.read_csv('data/palmer-total.csv')
+whelks = pd.read_csv('data/whelk_correlation_table.csv')
+
+print(whelks)
