3d386557e58bf111cb31cb31b2fb53ac95f50738
Anusha Datar <anushadatar@gmail.com>
2017-09-26 15:35:55 -0400

    Added modsim.py

--- plot_data.py
+++ plot_data.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python
 #Plots data points from article about whelk and barnacle populations.
 
-from ModSimPy.code.modsim import *
+from modsim import *
 from data import whelk_correlation_table
 
-print("Imported stuff.")
+print("Imported stuff....")
