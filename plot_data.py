3398a967d64813f08d5428dbef457c01ff35b460
Anusha Datar <anushadatar@gmail.com>
2017-09-26 16:27:06 -0400

    Pivoting to wolves...

--- plot_data.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-#Plots data points from article about whelk and barnacle populations.
-
-from modsim import *
-from data import whelk_correlation_table
-
-print("Imported stuff....")
