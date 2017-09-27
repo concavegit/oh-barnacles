88ba35d4867119e87e23d61f4e972ffc20ff4e20
Kawin Nikomborirak <concavemail@gmail.com>
2017-09-27 19:24:07 +0000

    add more wolf data

--- model.py
+++ model.py
@@ -3,8 +3,4 @@ import matplotlib.pyplot as plt
 
 plt.style.use('ggplot')
 
-palmerAlive = pd.read_csv('data/palmer-alive.csv')
-palmerDead = pd.read_csv('data/palmer-dead.csv')
-palmerTotal = pd.read_csv('data/palmer-total.csv')
-whelks = pd.read_csv('data/whelk_correlation_table.csv')
-
+wolves = pd.read_csv('data/wolf-data.csv').set_index('Year')
